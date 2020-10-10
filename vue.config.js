98befcbd6f529da5d366a974e9522257698ae6bb
Your Name <you@example.com>
2020-10-10 18:41:32 +0300

    bugfixes

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,5 @@ module.exports = {
   devServer: {
     proxy: 'https://www.metaweather.com'
   },
-  assetsDir: '',
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: ' @import "@/assets/styles/styles.scss"; '
-      }
-    }
-  }
+  assetsDir: ''
 }
